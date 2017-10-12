409cc9d1939021947105f7340c43ad505c2dc973
Joycie Yu <joycie@uw.edu>
2017-10-12 16:19:08 -0700

    change from double to int

--- CalcArray/main.swift
+++ CalcArray/main.swift
@@ -8,27 +8,23 @@
 
 import Foundation
 
-func add (_ num1: Double, _ num2: Double) -> Double {
+func add (_ num1: Int, _ num2: Int) -> Int {
     return num1 + num2
 }
 
-func subtract (_ num1: Double, _ num2: Double) -> Double {
+func subtract (_ num1: Int, _ num2: Int) -> Int {
     return num1 - num2
 }
 
-func multiply (_ num1: Double, _ num2: Double) -> Double {
+func multiply (_ num1: Int, _ num2: Int) -> Int {
     return num1 * num2
 }
 
-func divide (_ num1: Double, _ num2: Double) -> Double {
+func divide(_ num1: Int, _ num2: Int) -> Int {
     return num1 / num2
 }
 
-func mod (_ num1: Double, _ num2: Double) -> Double {
-    return num1.truncatingRemainder(dividingBy: num2)
-}
-
-func simpleOps (_ num1: Double, _ num2: Double, _ operation: String) -> Double{
+func simpleOps (_ num1: Int, _ num2: Int, _ operation: String) -> Int{
     switch operation {
     case "+", "add" :
         return add(num1, num2)
@@ -38,42 +34,40 @@ func simpleOps (_ num1: Double, _ num2: Double, _ operation: String) -> Double{
         return multiply(num1, num2)
     case "/", "divide" :
         return divide(num1, num2)
-    case "%", "mod" :
-        return mod(num1, num2)
     default:
-        return -1.0
+        return -1
     }
 }
 
-func add (_ numArr : [Double]) -> Double{
-    var result = 0.0
+func add (_ numArr : [Int]) -> Int {
+    var result = 0
     for index in 0...(numArr.count - 1){
-        result += Double(numArr[index])
+        result += numArr[index]
     }
     return result
 }
 
-func multiply (_ numArr: [Double]) -> Double {
-    var result = 0.0
+func multiply (_ numArr : [Int]) -> Int {
+    var result = 0
     for index in 0...(numArr.count - 1){
-        result *= Double(numArr[index])
+        result *= numArr[index]
     }
     return result
 }
 
-func count (_ numArr: [Double]) -> Double {
-    return Double(numArr.count - 1)
+func count (_ numArr : [Int]) -> Int {
+    return numArr.count - 1
 }
 
-func avg (_ numArr: [Double]) -> Double {
-    var result = 0.0
+func avg (_ numArr : [Int]) -> Int {
+    var result = 0
     for index in 0...(numArr.count - 1){
-        result += Double(numArr[index])
+        result += numArr[index]
     }
-    return result/Double(numArr.count)
+    return result/numArr.count
 }
 
-func arrOps (_ numArr: [Double], _ operation: String) -> Double {
+func arrOps (_ numArr: [Int], _ operation: String) -> Int {
     switch operation {
     case "+", "add" :
         return add(numArr)
@@ -84,22 +78,24 @@ func arrOps (_ numArr: [Double], _ operation: String) -> Double {
     case "avg" :
         return count(numArr)
     default :
-        return -1.0
+        return -1
     }
 }
 
-func add (_ nums1: (x: Double, y: Double), _ nums2: (x: Double, y: Double) ) -> (Double, Double) {
+func add (_ nums1: (x: Int, y: Int), _ nums2: (x: Int, y: Int)) -> (Int, Int) {
     return (nums1.x + nums2.x, nums1.y + nums2.y)
 }
 
-func subtract (_ nums1: (x: Double, y: Double), _ nums2: (x: Double, y: Double) ) -> (Double, Double) {
+func subtract (_ nums1: (x: Int, y: Int), _ nums2: (x: Int, y: Int) ) -> (Int, Int) {
     return (nums1.x - nums2.x, nums1.y - nums2.y)
 }
 
-func add (_ nums1:[String: Double], _ nums2: [String: Double]) -> [String: Double] {
-    var results : [String : Double] = [:]
-    results.updateValue(nums1["x"]! + nums2["x"]!, forKey:"x")
-    results.updateValue(nums1["y"]! + nums2["y"]!, forKey:"y")
+func add (_ nums1:[String: Int], _ nums2: [String: Int]) -> [String: Int] {
+    var results : [String : Int] = [:]
+    if (nums1["x"] != nil && nums2["x"] != nil && nums1["y"] != nil && nums2["y"] != nil) {
+        results.updateValue(nums1["x"]! + nums2["x"]!, forKey:"x")
+        results.updateValue(nums1["y"]! + nums2["y"]!, forKey:"y")
+    }
     return results
 }
 
